9ecc08aaddecabb79fd087947177c084d020553f
Adnan BAL <adnbal89@hotmail.com>
2022-02-07 18:01:32 +0300

    Part 4 - API Interface  - API Interface (retrofit) implemented.  - nav_graph created  - UnsplashResponse.kt created.

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,9 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+//inflate the layout
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+}
