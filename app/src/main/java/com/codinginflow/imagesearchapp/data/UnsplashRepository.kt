950e8d5e47f7680390efb0eced7ee51256e198ca
Adnan BAL <adnbal89@hotmail.com>
2022-02-08 11:01:21 +0300

    Part 6 - Paging Data  - Pagination mechanism implemented

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.codinginflow.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.codinginflow.imagesearchapp.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -8,6 +11,19 @@ import javax.inject.Singleton
 class UnsplashRepository @Inject constructor(
     private val unsplashApi: UnsplashApi
 ) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                UnsplashPagingSource(unsplashApi, query)
+            }
+            //can be used flow here.
+        ).liveData
 
 
 }
--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,11 +1,24 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearchapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 //inflate the layout
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    private val viewModel: GalleryViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+
+        }
+    }
 
 }
